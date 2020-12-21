6248bcc11e7da1976d61a7ac7c461e836c18df06
Daniel Martin Gonzalez <danimart1991@outlook.com>
2020-12-21 15:46:17 +0100

    Adapt code to danimart1991/sonarr

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "List installed packages..."
+docker run --rm --entrypoint="" "${1}" apt list --installed
+echo "Check if app works..."
+app_url="http://localhost:8989/system/status"
+docker run --rm --network host -d --name service -e DEBUG="yes" "${1}"
+currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+curl -fsSL "${app_url}" > /dev/null
+status=$?
+echo "Show docker logs..."
+docker logs service
+exit ${status}
